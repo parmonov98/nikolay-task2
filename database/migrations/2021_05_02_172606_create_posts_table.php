e5044eb5cad9b314017517f0b1674545627afea3
Murod <parmonov98@yandex.ru>
2021-05-02 22:47:15 +0500

    task2 is done. I think

---
+++ database/migrations/2021_05_02_172606_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedMediumInteger('parent_id')->nullable(); // если это пост\статья то некоторие пости может и не иметь родительский пост
+            $table->string('name', 20);
+            $table->string('slug', 20); // for url
+            $table->string('description');
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
